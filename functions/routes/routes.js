e1f22f9856247b5687be0415a54a99095c20b7ff
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-09 18:27:35 -0300

    feat(be11): add the new routes to listen to the calls for the platform and product endpoints

--- functions/routes/routes.js
+++ functions/routes/routes.js
@@ -1,12 +1,16 @@
-const {Router} = require('express');
-const mercadoPagoRoutes = require('./mpRoute');
-const addressRoutes = require('./addressRoute');
-const cartRoutes = require('./cartRoutes');
+const { Router } = require("express");
+const mercadoPagoRoutes = require("./mpRoute");
+const addressRoutes = require("./addressRoute");
+const cartRoutes = require("./cartRoutes");
+const productRoutes = require("./productRoute");
+const platformRoutes = require('./platformRoute');
 
 const router = Router();
 
 router.use(mercadoPagoRoutes);
 router.use(addressRoutes);
 router.use(cartRoutes);
+router.use(productRoutes);
+router.use(platformRoutes);
 
 module.exports = router;
