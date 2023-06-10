ade4cfc83bf89b66079624d2232056a25bf3d722
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-10 16:05:42 -0300

    feature(be2): add endpoint for cart management so the frontend will be able to add, remove, modify and get the list of items within a cart

--- functions/routes/cartRoutes.js
+++ functions/routes/cartRoutes.js
@@ -1,10 +1,22 @@
 const { Router } = require("express");
 const { fieldValidator } = require("../middlewares/fieldValidators");
-const { getCart } = require("../controllers/cart");
+const {
+  getCart,
+  cleanCart,
+  addToCart,
+  removeFromCart,
+} = require("../controllers/cart");
 
 const router = Router();
 
 // [GET]
 router.get("/carts", [fieldValidator], getCart);
+
+// [POST]
+router.post("/addToCart", [fieldValidator], addToCart);
+
+// [DELETE]
+router.delete("/cleanCart", [fieldValidator], cleanCart);
+router.delete("/removeFromCart", [fieldValidator], removeFromCart);
 
 module.exports = router;
