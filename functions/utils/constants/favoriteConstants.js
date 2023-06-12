693d2f0d62a89b6ac18110039ec372fbd1a4d658
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-12 07:36:20 -0300

    chore(be7): add constants used by the favorite controller to avoid using magic strings

---
+++ functions/utils/constants/favoriteConstants.js
@@ -0,0 +1,11 @@
+const ERROR_FINDING_FAVORITES =
+  "Error when trying to verify if the product is a favorite";
+const ERROR_SAVING_FAVORITE = "Error trying to save the item as a favorite";
+const ERROR_GETTING_FAVORITE_LIST =
+  "Error trying to get favorites list of the user";
+
+module.exports = {
+  ERROR_FINDING_FAVORITES,
+  ERROR_SAVING_FAVORITE,
+  ERROR_GETTING_FAVORITE_LIST,
+};
