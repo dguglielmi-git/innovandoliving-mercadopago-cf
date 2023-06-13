3ccc708ef26407ca368fc07cfec015bd2e6b7cae
Daniel Guglielmi <dguglielmi.asm@gmail.com>
2023-06-13 18:30:34 -0300

    refactor(be21): remove token validation when consuming platform endpoint since it is used from the main page even when there is no user logged in

--- functions/controllers/platform.js
+++ functions/controllers/platform.js
@@ -1,13 +1,10 @@
 const Platform = require("../models/platform");
-const { REQUEST_WITHOUT_TOKEN } = require("../utils/constants");
 const {
   ERROR_GETTING_PRODUCTS,
 } = require("../utils/constants/productConstants");
-const { getValidToken, errorWithoutToken } = require("../utils/utils");
 
 const getPlatforms = async (req, res) => {
   try {
-    await getValidToken(req);
     const { sort } = req.query;
     const [position, order] = sort.split(":");
     const sortOrder = order === "-1" ? -1 : 1;
@@ -16,9 +13,6 @@ const getPlatforms = async (req, res) => {
     return res.json(platformList);
   } catch (error) {
     console.error(error);
-    if (error === REQUEST_WITHOUT_TOKEN) {
-      return errorWithoutToken(res);
-    }
     return res.json({
       response: `${ERROR_GETTING_PRODUCTS} - error: ${error}`,
     });
